6203e2587a03be0be9351007e0f334873a68c73f
ChoudhariNikita <choudharinikita96@gmail.com>
2024-08-27 11:02:28 +0530

    About page removed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 // import PropTypes from 'prop-types';
 
 export default function Navbar(props) {
@@ -9,9 +8,9 @@ export default function Navbar(props) {
         className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} `}
       >
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
+          <a className="navbar-brand" href="/">
             {props.title}
-          </Link>
+          </a>
           <button
             className="navbar-toggler"
             type="button"
@@ -26,18 +25,9 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
+                <a className="nav-link active" aria-current="page" href="/">
                   Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className="nav-link"
-                  to="/about"
-                
-                >
-                  About
-                </Link>
+                </a>
               </li>
             </ul>
           </div>
